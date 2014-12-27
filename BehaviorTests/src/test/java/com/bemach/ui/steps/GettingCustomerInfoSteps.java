734cfd191983c1203555134bc787a6960f24071e
drtran <ktran@bemach.com>
2014-12-27 09:43:29 -0500

    Added Behavior Tests to UI and Restful apps

--- BehaviorTests/src/test/java/com/bemach/ui/steps/GettingCustomerInfoSteps.java
+++ BehaviorTests/src/test/java/com/bemach/ui/steps/GettingCustomerInfoSteps.java
@@ -1,48 +1,88 @@
 package com.bemach.ui.steps;
 
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.bemach.restful.jersey.steps.Customer;
+import com.bemach.restful.jersey.steps.RSClient;
+
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class GettingCustomerInfoSteps {
-	@Given("^that I am an authorized user \"([^\"]*)\" with password \"([^\"]*)\"$")
-	public void that_I_am_an_authorized_user_with_password(String arg1, String arg2) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
-	}
+	private String userName;
+	private String password;
+	private WebDriver driver;
+	private String url;
+	private String restURL;
 
-	@Given("^that the customer exists with:$")
-	public void that_the_customer_exists_with(DataTable arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    // For automatic conversion, change DataTable to List<YourType>
-	    throw new PendingException();
+	@Before
+	public void setUp() {
+		driver = new ChromeDriver();
 	}
+	
+	@After
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+	
+	@Given("^that I am an authorized user \"([^\"]*)\" with password \"([^\"]*)\"$")
+	public void that_I_am_an_authorized_user_with_password(String userName, String password) throws Throwable {
+		this.userName = userName;
+		this.password = password;
+		this.url = System.getProperty("URL");
+		this.restURL = System.getProperty("REST_URL");
+		driver.get(url);
+		assertEquals("Northwind Web App", driver.getTitle());
+	}
+	
+	@Given("^that the customer exists with \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", and \"([^\"]*)\":$")
+	public void that_the_customer_exists_with_and_(String customerId, String companyName, 
+				String contactName, String contactTitle, String phone) throws Throwable {
+		RSClient rsClient = new RSClient();
+		rsClient.setUrl(restURL);
+		Customer customer = rsClient.getCustomerById(customerId);
+		assertNotNull(customer);
+	}
+	
 
 	@When("^I am at Customer Information page$")
 	public void I_am_at_Customer_Information_page() throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+	    WebElement customerLink = driver.findElement(By.id("customersPage"));
+	    customerLink.click();
+	    assertEquals("Customer information", driver.getTitle());
 	}
 
 	@When("^I enter the \"([^\"]*)\"$")
-	public void I_enter_the(String arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+	public void I_enter_the(String customerId) throws Throwable {
+		WebElement custIdInput = driver.findElement(By.id("customerIdInput"));
+		custIdInput.sendKeys(customerId);
 	}
 
 	@When("^I click on a retrieve button$")
 	public void I_click_on_a_retrieve_button() throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+		WebElement retrieveButton = driver.findElement(By.id("retrieveButton"));
+		retrieveButton.click();
 	}
 
 	@Then("^I should see a customer record with \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\", and \"([^\"]*)\"$")
-	public void I_should_see_a_customer_record_with_and(String arg1, String arg2, String arg3, String arg4, String arg5, DataTable arg6) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	    // For automatic conversion, change DataTable to List<YourType>
-	    throw new PendingException();
+	public void I_should_see_a_customer_record_with_and(String customerId, String companyName, 
+			String contactName, String contactTitle, String phone) throws Throwable {
+		assertEquals(customerId, driver.findElement(By.id("customerId")).getText());
+		assertEquals(companyName, driver.findElement(By.id("companyName")).getText());
+		assertEquals(contactName, driver.findElement(By.id("contactName")).getText());
+		assertEquals(contactTitle, driver.findElement(By.id("contactTitle")).getText());
+		assertEquals(phone, driver.findElement(By.id("phone")).getText());
 	}
 
 
--- BehaviorTests/src/test/resources/com/bemach/ui/features/GetCustomerInfo.feature
+++ BehaviorTests/src/test/resources/com/bemach/ui/features/GetCustomerInfo.feature
@@ -4,16 +4,15 @@ Feature: Getting Customer Information Application
     I want to get information of a customer by a customer ID
     So that, I can send promotion information to that customer.
     
-  Scenario: Getting a customer record by a given customer ID
+  Scenario Outline: Getting a customer record by a given customer ID
     Given that I am an authorized user "ws_user" with password "user1234"
-    And that the customer exists with:
-      | customer_id | company_name       | contact_name   | contact_title | phone          |      
-      | EASTC       | Eastern Connection | Ann Devon      | Sales Agent   | (171) 555-0297 |
-      | DUMON       | Du monde entier    | Janine Labrune | Owner         | France         |    
+    And that the customer exists with "<customer_id>", "<company_name>", "<contact_name>", "<contact_title>", and "<phone>":
     When I am at Customer Information page
     And I enter the "<customer_id>"
     And I click on a retrieve button
     Then I should see a customer record with "<customer_id>", "<company_name>", "<contact_name>", "<contact_title>", and "<phone>"
+    
+    Examples:
       | customer_id | company_name       | contact_name   | contact_title | phone          |      
       | EASTC       | Eastern Connection | Ann Devon      | Sales Agent   | (171) 555-0297 |
       | DUMON       | Du monde entier    | Janine Labrune | Owner         | France         |
